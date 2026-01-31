1503a3bae60bb245d7bb94e8631846d0c6e5a58f
Fllavex <mashtalermaksim2012@gmail.com>
2026-01-31 14:30:53 +0200

    add css

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const inputText = document.getElementById('input');
+    const translateBtn = document.getElementById('translateBtn');
+    const outputText = document.getElementById('output');
+    async function translateText() {
+
+
+
+
+    
+});
