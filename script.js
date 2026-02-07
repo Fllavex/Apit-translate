9cd0e97a460bf3e24d99d108b826d3411d198abf
Fllavex <mashtalermaksim2012@gmail.com>
2026-02-07 13:51:37 +0200

    final

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const inputText = document.getElementById('input');
     const translateBtn = document.getElementById('translateBtn');
     const outputText = document.getElementById('output');
+
+
     async function translateText() {
+        const text = inputText.value.trim();
+        const safeText = encodeURIComponent(text);
+        const email = 'mashtalermaksim2012@gmail.com';
+        const url = `https://api.mymemory.translated.net/get?q=${safeText}&langpair=uk|en&de=${email}`;
 
+        const response = await fetch(url);
+        const data = await response.json();
 
+        console.log(data);
+
+        outputText.textContent = data.responseData.translatedText;
 
 
     
+}
+translateBtn.addEventListener('click', translateText);
 });
